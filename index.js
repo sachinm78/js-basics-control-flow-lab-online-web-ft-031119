a29ab045bd16b4f954886b59196d5cc76d722f29
Sachin Mahajan <sachinm78@gmail.com>
2019-06-10 22:03:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function scuberGreetingForFeet(distance){
   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? ("Ok, sounds good.") : ("No go.");
 }
 
 function switchOnCharmFromTip(){
