b23386c870ea06703a75cde3cacdd333e0c15dba
Sachin Mahajan <sachinm78@gmail.com>
2019-06-10 22:04:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function ternaryCheckCity(city){
   return city === "NYC" ? ("Ok, sounds good.") : ("No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
 }
